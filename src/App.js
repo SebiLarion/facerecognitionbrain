993d624a26a410215c7c4ec1ccebdd4f43d460a5
PETRU EUSEBIU LARION <eusebiu.larion@rcs-rds.local>
2020-08-10 14:51:10 +0300

    another commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,96 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import Navigation from './components/navigation/Navigations';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
+
 import './App.css';
 
-function App() {
+const app= new Clarifai.App({
+  apiKey: '703e5d120b6a4d3b8ea4619e6722e3da'
+});
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+
+}
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl:'',
+      box:{},
+    }
+  }
+
+  calculatedFaceLocation = (data) =>{
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage')
+    const width = Number(image.width);
+    const height = Number(image.height);
+    console.log(width, height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width-(clarifaiFace.right_col * width),
+      bottomRow: height-(clarifaiFace.bottom_row * height)
+    }
+
+  }
+
+  displayFaceBox = (box) =>{
+    console.log(box);
+    this.setState({box:box})
+  }
+
+  onInputChange = (event) =>{
+    this.setState({input:event.target.value});
+
+  } 
+
+  onButtonSubmit = () =>{
+    this.setState({imageUrl:this.state.input})
+    app.models
+    .predict(
+       Clarifai.FACE_DETECT_MODEL, 
+      this.state.input)
+    .then(response => this.displayFaceBox(this.calculatedFaceLocation(response)))
+    .catch(err => console.log(err)) ;
+    
+  }
+
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Particles className='particles'
+          params={particlesOptions}     	
+      />
+      <Navigation />
+      <Logo />
+      <Rank />
+      <ImageLinkForm  
+        onInputChange={this.onInputChange} 
+        onButtonSubmit={this.onButtonSubmit}
+      />
+      <FaceRecognition imageUrl={this.state.imageUrl}/> 
+
     </div>
   );
+}
 }
 
 export default App;
